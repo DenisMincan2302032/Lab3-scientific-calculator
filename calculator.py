5a12e7ab3678844be405544382d6c3f683e02384
Denis Mincan <2302032@leedstrinity.ac.uk>
2024-10-11 10:46:52 +0100

    Version 1 of calculator

---
+++ calculator.py
@@ -0,0 +1,99 @@
+import math
+from math import sin, cos, tan
+def add():
+    number = int(input("Please enter the first number to your addition:"))
+    number1 = int(input("Plese enter the second number to your addition:"))
+    numresult = number + number1 
+    print("Your operation result is", numresult)
+
+
+
+def subtract():
+    subnum = int(input("Please enter the first number to your subtraction:"))
+    subnum1 = int(input("Please enter the second number to your subtraction:"))
+    subresult = subnum + subnum1
+    print("Your operation result is", subresult)
+
+
+def multiply():
+    multnumber = int(input("Please enter the first number to your multiplication:"))
+    multnumber1 = int(input("Please enter the second number to your multiplication:"))
+    multresult = multnumber * multnumber1
+    print("Your operation result is", multresult)
+
+def div():
+    divnum = int(input("Please enter the first number to your division:"))
+    divnum1 = int(input("Please enter the second number to your division:"))
+    try:
+        print(divnum/divnum1)
+    except ZeroDivisionError:
+        print("Error. It is mathematically impossible to divide by zero.")
+
+def square():
+    squarenum = int(input("Please enter the number to find the square root for:"))
+    squareresult = math.sqrt(squarenum)
+    print("The result of your operation is", squareresult)
+
+
+
+def sin():
+    sin_number = float(input("Please enter the number for your sin operation:"))
+    sin_number_radian = math.radians(sin_number)
+    sin_result = math.sin(sin_number_radian)
+    print("The sine of", sin_number, "is", sin_result)
+
+def cos():
+    cos_number = float(input("Please enter the number for your cos operation:"))
+    cos_number_radian = math.radians(cos_number)
+    cos_result = math.cos(cos_number_radian)
+    print("The cosine of", cos_number, "is", cos_result)
+
+
+def tan():
+    tan_number = float(input("Please enter the number for your tan operation:"))
+    tan_number_radian = math.radians(tan_number)
+    tan_result = math.tan(tan_number_radian)
+    print("The tangent of", tan_number, "is", tan_result)
+
+def main_menu():
+    while True:
+        print("Welcome to the Scientific Calculator Program")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Square Root")
+        print("6. Sine")
+        print("7. Cosin")
+        print("8.Tangent")
+        userchoice = input("Enter your choice")
+        if userchoice == "1":
+            add()
+        elif userchoice == "2":
+            subtract()
+        elif userchoice == "3":
+            multiply()
+        elif userchoice == "4":
+            div()
+        elif userchoice == "5":
+            square()
+        elif userchoice == "6":
+            sin()
+        elif userchoice == "7":
+            cos()
+        elif userchoice == "8":
+            tan()
+        else:
+            print("Invalid option, try again")
+            continue
+        exit = input("Would you like to exit the program? y/n")
+        if exit == "y":
+            print("Goodbye")
+            break
+        else:
+            continue
+
+main_menu()
+
+
+
